03cc0be34c8af7b2a5c3e8d616b5f670a921bcd3
SM2201DD <SM2201DD@DESKTOP-2BAN41Q>
2022-04-15 17:04:55 +0900

    연결

--- GitTest/src/main/java/Ex01.java
+++ GitTest/src/main/java/Ex01.java
@@ -6,8 +6,6 @@ public class Ex01 {
 		System.out.println("첫번째 커밋");
 		System.out.println("두번째 커밋");
 		System.out.println("세번째 커밋");
-		System.out.println("네번째 커밋");
-		System.out.println("다섯번째 커밋");
 		
 	}
 
